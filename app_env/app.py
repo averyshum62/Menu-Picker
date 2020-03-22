52a2af3a86262d53704e1af1f87f24dd4e97f60a
Michael Zheng <cm2zheng@edu.uwaterloo.ca>
2020-03-22 00:53:58 -0400

    commit to michael's dev branch

--- app_env/app.py
+++ app_env/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World"
+    return "Hello World! This is a test to commit to Michael's dev remote branch"
 
 
 if __name__ == '__main__':
